beb046dbfd0dda5dc27957d0fa13c9fc5f3d70e3
Malyck Al Mamamri <malyck.almamari@gmail.com>
2023-07-24 05:50:41 +0400

    update server

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(cors());
 
+projectData = {};
+
 const server = app.listen(port,listening);
 
 function listening() {
